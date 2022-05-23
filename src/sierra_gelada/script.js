2030ef89fdc183432d34e51b8cba7a285555b930
Jose Manuel Mira Martínez <josema.mira@gmail.com>
2022-05-23 14:04:59 +0200

    Add files via upload

--- src/sierra_gelada/script.js
+++ src/sierra_gelada/script.js
@@ -43,7 +43,7 @@ let osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 let topograficoICV = L.tileLayer( "https://terramapas.icv.gva.es/mapabase_topografico/wmts/1.0.0/topografico_continuo_epsg3857/default/GMEPSG3857/{z}/{y}/{x}.png" ,{
 	maxNativeZoom: 19,
 	maxZoom: 19,
-	attribution: "| <a href='https://idev.gva.es/es'>IDEV&copy;ICV</a> | Develop by <a href='https://github.com/josemamira'>José Manuel Mira</a>"
+	attribution: "<a href='https://idev.gva.es/es'>IDEV</a>&copy;ICV"
 	}
 );
 
@@ -62,8 +62,8 @@ var baseMaps = {
 };
 
 var map = L.map('map', {
-	center: [38.5645,-0.0503],
-	zoom: 16,
+	center: [39,0],
+	zoom: 8,
 	//maxZoom:19,
 	scrollWheelZoom: false,
 	layers: [esriLayer,topograficoICV]
